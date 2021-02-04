9ff3d4bd2a51dc7438d620e9045f4257d974a33e
komalerkanappan <45581503+komalerkanappan@users.noreply.github.com>
2021-02-04 12:14:25 +0800

    Create HomeScreen.js

---
+++ netflix-clone-2.0/src/HomeScreen.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+
+function HomeScreen() {
+  return (
+    <div className="homescreen">
+      <Nav />
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+      <h1>fhdvdh</h1>
+
+      {/* Banner */}
+
+      {/*  */}
+    </div>
+  );
+}
+
+export default HomeScreen;
