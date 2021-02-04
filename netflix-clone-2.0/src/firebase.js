0f8d91dad932e52e2ff8851e64d8028d39678bec
komalerkanappan <45581503+komalerkanappan@users.noreply.github.com>
2021-02-04 12:14:19 +0800

    Create firebase.js

---
+++ netflix-clone-2.0/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBhwc57nXD4M6UlF_faD-yXiGgHTsYYhfk",
+  authDomain: "netflix-clone-2-12292.firebaseapp.com",
+  projectId: "netflix-clone-2-12292",
+  storageBucket: "netflix-clone-2-12292.appspot.com",
+  messagingSenderId: "356555683694",
+  appId: "1:356555683694:web:9e79c5e69b1d5b9ab3337f",
+};
