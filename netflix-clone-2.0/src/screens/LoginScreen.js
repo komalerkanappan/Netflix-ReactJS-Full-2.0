855c06624a575d3fdc5726cd8432ec502b9b1f9b
komalerkanappan <45581503+komalerkanappan@users.noreply.github.com>
2021-03-01 11:56:29 +0800

    updated

---
+++ netflix-clone-2.0/src/screens/LoginScreen.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useState } from "react";
+import "./LoginScreen.css";
+import SignupScreen from "./SignupScreen.js";
+
+function LoginScreen() {
+  const [signIn, setSignIn] = useState(false);
+
+  return (
+    <div className="loginScreen">
+      <div className="loginScreen__background">
+        <img
+          className="loginScreen__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+          alt="Netflix Logo"
+        />
+        <button onClick={() => setSignIn(true)} className="loginScreen__button">
+          Sign In
+        </button>
+        <div className="loginScreen__gradient" />
+      </div>
+      <div className="loginScreen__body">
+        {signIn ? (
+          <SignupScreen />
+        ) : (
+          <>
+            <h1>Movies, unlimited TV shows and more.</h1>
+            <h2>Watch everywhere. Make cancellations at any time.</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </h3>
+            <div className="loginScreen__input">
+              <form>
+                <input type="email" placeholder="Email Address" />
+                <button
+                  onClick={() => setSignIn(true)}
+                  className="loginScreen__getStarted"
+                >
+                  GET STARTED
+                </button>
+              </form>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default LoginScreen;
